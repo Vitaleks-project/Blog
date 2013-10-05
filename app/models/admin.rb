6e8b3498a00c8f0b6354f700f15f7ee872523ab5
Vitaleks-project <vitaleks2010@i.ua>
2013-10-05 19:21:47 +0300

    Wrong

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,7 @@
 class Admin < ActiveRecord::Base
+
+  devise :database_authenticatable, :registerable,
+               :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :email, :password, :password_confirmation, :remember_me
   has_many :posts
 end
