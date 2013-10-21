296611f3d42db2fb22685454c889171c0ec0b27f
Vitaleks-project <vitaleks2010@i.ua>
2013-10-21 17:58:14 +0300

    Create admin area users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,7 @@
 class AdminController < ApplicationController
   before_filter :authenticate_admin!
   def home
+    @admins = Admin.order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
     @users = User.order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
     @posts = Post.order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
     @clubs = Club.order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
