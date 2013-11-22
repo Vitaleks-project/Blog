cde69bb7b15bcd6a28f80d1b9d684feb7e940614
Vitaleks-project <vitaleks2010@i.ua>
2013-11-22 16:21:58 +0200

    Back factory players

---
+++ spec/factories/players_factory.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :player do
+    first_name "Cristiano"
+    last_name "Ronaldo"
+    birthday "10-15-1988"
+    country "Portugal"
+    club "Real Madrid"
+    admin_id 1
+    position "ФРВ"
+    number 7
+  end
+end
