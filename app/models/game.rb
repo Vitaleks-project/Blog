76853f74da6fe9bd4ea3befb1875ea50d0d03f1f
Vitaleks-project <vitaliy.jorzh100592@gmail.com>
2013-11-29 15:29:58 +0200

    Add message and fix method

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
   attr_accessible :admin_id, :location, :total, :club_ids
   belongs_to :admin
-  validates :admin_id, :location, :total, :club_ids, :presence => true
+  validates :admin_id, :location, :total, :presence => true
   has_and_belongs_to_many :clubs
 
   before_save :only_two_clubs
@@ -9,7 +9,8 @@ class Game < ActiveRecord::Base
   def only_two_clubs
     if club_ids.size == 2
       return true
-    else
+    elsif(club_ids.size == 1 || club_ids.size > 2 || club_ids.size == 0 )
+      errors.add(:base, 'You must select two clubs for game')
       return false
     end
   end
