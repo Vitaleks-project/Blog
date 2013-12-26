2ab4a3f3c19127924c4e24dc4696588fd30e6f88
Vitaleks-project <vitaliy.jorzh100592@gmail.com>
2013-12-26 17:08:08 +0200

    Add search for post root page

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -5,5 +5,12 @@ class PageController < ApplicationController
     else
       @posts = Post.order('created_at DESC').paginate(:page => params[:page], :per_page => 7)
     end
+
+    if params[:search]
+      @posts = Post.search(params[:search]).paginate(:page => params[:page])
+      flash[:error] = "Not found." if params[:search] == ""
+    else
+      @posts = Post.order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
+    end
   end
 end
